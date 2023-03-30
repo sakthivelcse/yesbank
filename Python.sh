e0e9f2f7a94ac9165a17875ac78b915fe044eb16
krish92vel <vel92cse@gmail.com>
2023-03-30 14:07:02 +0000

    updated

--- Python.sh
+++ Python.sh
@@ -2,10 +2,14 @@ hi heelnflndf
 sfbjfgljsbgjs
 fjsdbjsdbfljs
 <<<<<<< HEAD
+<<<<<<< HEAD
 gsfngjnslgj
 
 
 hi my name is sakthivel updated here
+=======
+gsfngjnslgjn
+>>>>>>> parent of e3cc37b (updated)
 =======
 gsfngjnslgjn
 >>>>>>> parent of e3cc37b (updated)
